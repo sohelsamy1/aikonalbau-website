2b76da6cc69db45c53d7ecc760718f246b5bc951
Sohel Samy <sumon33021@gmail.com>
2026-09-08 12:41:32 +0600

    feat: add Next.js project files

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-zinc-50 text-zinc-900 flex flex-col justify-between font-sans overflow-x-hidden">
+      
+    </div>
+  );
+}
